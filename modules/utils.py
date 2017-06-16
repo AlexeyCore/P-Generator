aa72eff4f69ef770e40976382ad9c1f8018a191e
Bogaley Alexey <vuraddo@yahoo.com>
2017-06-16 11:00:55 +0700

    Clean utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -14,10 +14,3 @@ def run_command(commands=['']):
         print (commands_line)
         output = sp.run(commands_line, stdout=sp.PIPE, shell=True)
         return output.stdout.decode('utf-8')
-
-# def subprocess_cmd(command):
-#     process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
-#     proc_stdout = process.communicate()[0].strip()
-#     print proc_stdout
-#
-# subprocess_cmd('echo a; echo b')
