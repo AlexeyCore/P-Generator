ebc6a8c26ee27bbd68f2f5faf722ef273b71dc1d
Bogaley Alexey <vuraddo@yahoo.com>
2017-06-20 11:10:09 +0700

    restructure_file_content

--- packages/modules/structure.py
+++ packages/modules/structure.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, re
 from packages.modules.state import g_state
 from distutils.dir_util import copy_tree
 
@@ -35,6 +35,25 @@ def create_project_dir():
 def copy_template(path=''):
     dst = '%s/%s' % (g_state.get('projects_path'), g_state.get('name'))
     copy_tree(path, dst)
+
+
+# TODO: get files sheme from template path
+def restructure_file_content(files=[], t_vars={}):
+    reg_ex = r'%{(.*?)}%'
+    files_path = '%s/%s' % (g_state.get('projects_path'), g_state.get('name'))
+    for file_path in files:
+        file = open('%s/%s' % (files_path, file_path), 'r+')
+        f_content = file.read()
+        template_values = re.findall(reg_ex, f_content)
+        if template_values:
+            for template_value in template_values:
+                if template_value in t_vars:
+                    f_content = re.sub(r'%{%s}%' % template_value, t_vars[template_value], f_content)
+                else:
+                    f_content = re.sub(r'%{%s}%' % template_value, '', f_content)
+                file.seek(0)
+                file.truncate()
+                file.write(f_content)
 
 
 def restructuring():
